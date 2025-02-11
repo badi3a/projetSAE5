d161baa205f74e940b63d8c9e5095582ddc08920
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2025-02-11 15:34:14 +0100

    update project

--- microservices/candidat/src/main/java/esprit/candidat/CandidatApplication.java
+++ microservices/candidat/src/main/java/esprit/candidat/CandidatApplication.java
@@ -3,9 +3,11 @@ package esprit.candidat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CandidatApplication {
 
 	public static void main(String[] args) {
